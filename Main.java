09715548391cce7dca9835b0dc43326027a10dcc
Nour A Alghamdi <65490460+namacoconut@users.noreply.github.com>
2021-02-07 11:55:29 +0000

    comparesion

--- Main.java
+++ Main.java
@@ -5,7 +5,22 @@ class Main {
     System.out.println('A');
     System.out.println(true);
 
+// how to define a variable?
+// write the data type, name the variable, add = sign, write the value according to datatype and then semicolon ;
     int age = 1;
     System.out.println(age);
+
+      // لا تقم بمسح المتغير لأنه سيتم استخدامه في السؤال التالي
+        int number = 10 + 2;
+      System.out.println(number);
+       
+      boolean isEqual = number == 12;
+      System.out.println(isEqual);
+      
+			boolean isLesserthan = 'a' < 'w';
+      System.out.println(isLesserthan);
   }
+  
 }
+
+
