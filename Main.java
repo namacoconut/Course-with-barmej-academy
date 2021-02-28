e29349892bc0283e55a0fce80fa1d38f57569ea7
Nour A Alghamdi <65490460+namacoconut@users.noreply.github.com>
2021-02-28 00:47:56 +0000

    arrayList

--- Main.java
+++ Main.java
@@ -1,35 +1,52 @@
-class Main {
-  public static void main(String[] args) {
-    System.out.println(1);
-    System.out.println(2.5);
-    System.out.println('A');
-    System.out.println(true);
+// class Main {
+//   public static void main(String[] args) {
+//     System.out.println(1);
+//     System.out.println(2.5);
+//     System.out.println('A');
+//     System.out.println(true);
 
-// how to define a variable?
-// write the data type, name the variable, add = sign, write the value according to datatype and then semicolon ;
-    int age = 1;
-    System.out.println(age);
+// // how to define a variable?
+// // write the data type, name the variable, add = sign, write the value according to datatype and then semicolon ;
+//     int age = 1;
+//     System.out.println(age);
 
-      // لا تقم بمسح المتغير لأنه سيتم استخدامه في السؤال التالي
-        int number = 10 + 2;
-      System.out.println(number);
+//       // لا تقم بمسح المتغير لأنه سيتم استخدامه في السؤال التالي
+//         int number = 10 + 2;
+//       System.out.println(number);
        
-      boolean isEqual = number == 12;
-      System.out.println(isEqual);
+//       boolean isEqual = number == 12;
+//       System.out.println(isEqual);
       
-			boolean isLesserthan = 'a' < 'w';
-      System.out.println(isLesserthan);
-  }
+// 			boolean isLesserthan = 'a' < 'w';
+//       System.out.println(isLesserthan);
+//   }
   
-}
+// }
 
 
-// arrays:
-public class Main {
-    public static String[] get() {
-		// String [] نوع المصفوفة
-        String [] array = {"Barmej", "Ahmad", "Code" };      
+// // arrays:
+// public class Main {
+//     public static String[] get() {
+// 		// String [] نوع المصفوفة
+//         String [] array = {"Barmej", "Ahmad", "Code" };      
 		
-        return array;
+//         return array;
+//     }
+// }
+
+import java.util.ArrayList;
+
+public class Main {
+    public static ArrayList<String> get() {
+		
+        ArrayList<String> programmingLanguages = new ArrayList<String>();
+
+        programmingLanguages.add("Java");
+        programmingLanguages.add("Python");
+        programmingLanguages.add("Swift");
+
+        programmingLanguages.remove(2);
+        
+		return programmingLanguages;
     }
 }
